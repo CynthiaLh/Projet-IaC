790bb3bdd43faa2c96d180a19de7bb3935ce271d
Cynthia Lhuillier <cynthia.lhuillier@gmail.com>
2026-07-01 12:13:54 +0200

    Configure remote S3 backend for Terraform state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "ynov-iac-2026-tfstate-cynthia"
+    key    = "state/terraform.tfstate"
+    region = "eu-west-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
